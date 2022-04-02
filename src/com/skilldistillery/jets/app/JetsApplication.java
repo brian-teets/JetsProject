3891e384be6981559a01afdd6a25bec9b91a1642
Brian Teets <bwt17@msn.com>
2022-04-02 12:24:02 -0600

    Added sysout statements to displayUserMenu method in JetsApplication class

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -20,7 +20,15 @@ public class JetsApplication {
 	
 	public void displayUserMenu() {
 		
-		
+		System.out.println("1 List all aircraft in fleet");
+		System.out.println("2 Fly all aircraft");
+		System.out.println("3 View fastest aircraft");
+		System.out.println("4 View jet with longest range");
+		System.out.println("5 Load all Cargo Jets"); // needs to call loadCargo() method on all existing CargoPlane objects
+		System.out.println("6 Dogfight! "); 
+		System.out.println("7 Add an aircraft to the fleet");
+		System.out.println("8 Remove an aircraft from the fleet");
+		System.out.println("9 Quit"); 
 		
 	} // end of displayUserMenu method 
 
