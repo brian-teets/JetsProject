f62d2cf1089bb3c9d70ced026773c1e964744f48
Brian Teets <bwt17@msn.com>
2022-04-03 08:50:02 -0600

    Stubbed out empty methods in AirField to correspond to the menu of options in JetsApplication

--- src/com/skilldistillery/jets/entities/AirField.java
+++ src/com/skilldistillery/jets/entities/AirField.java
@@ -12,9 +12,8 @@ import java.util.List;
 public class AirField {
 	private List<Jet> fleet; // variable name could also be called jets based on UML
 
-
 	public AirField() {
-	
+
 		// TODO AirField constructor can call readFromFile method and pass in the txt
 		// file
 		fleet = new ArrayList<>();
@@ -33,25 +32,22 @@ public class AirField {
 				String model = row[1];
 				double speed = Double.parseDouble(row[2]);
 				int range = Integer.parseInt(row[3]);
-				long price = Long.parseLong(row[4]); 
-				
-				if(typeOfJet.equalsIgnoreCase("Passenger")) {
-					Jet passengerJet = new Passenger(model, speed, range, price); 
+				long price = Long.parseLong(row[4]);
+
+				if (typeOfJet.equalsIgnoreCase("Passenger")) {
+					Jet passengerJet = new Passenger(model, speed, range, price);
 					fleet.add(passengerJet);
-				}
-				else if(typeOfJet.equalsIgnoreCase("CargoPlane")) {
+				} else if (typeOfJet.equalsIgnoreCase("CargoPlane")) {
 					Jet cargoPlane = new CargoPlane(model, speed, range, price);
 					fleet.add(cargoPlane);
-				}
-				else if(typeOfJet.equalsIgnoreCase("FighterJet")) {
-					Jet fighterJet = new FighterJet(model, speed, range, price); 
+				} else if (typeOfJet.equalsIgnoreCase("FighterJet")) {
+					Jet fighterJet = new FighterJet(model, speed, range, price);
 					fleet.add(fighterJet);
-				}
-				else if(typeOfJet.equalsIgnoreCase("DeepSpaceUFO")) {
+				} else if (typeOfJet.equalsIgnoreCase("DeepSpaceUFO")) {
 					Jet UFO = new DeepSpaceUFO(model, speed, range, price);
-					fleet.add(UFO); 
+					fleet.add(UFO);
 				}
-				
+
 //				System.out.println(line); // for testing 
 			}
 			bufIn.close();
@@ -60,20 +56,52 @@ public class AirField {
 			e.printStackTrace();
 		}
 	}
-	
-	public void writeToFile(Writer fn) {
-		try {
-			BufferedWriter writer = new BufferedWriter( new FileWriter("jets2.txt") );
-			writer.write("Test output text"); 
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+//	public void writeToFile(Writer fn) {
+//		try {
+//			BufferedWriter writer = new BufferedWriter( new FileWriter("jets2.txt") );
+//			writer.write("Test output text"); 
+//			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+
+	public void removeFromFile() {
+
 	}
-	
+
 	public void listAllJets() {
 //		System.out.println( Jet );
 	}
 
-} 
+	public void flyAllJets() {
+
+	}
+
+	public void viewFastestJet() {
+
+	}
+
+	public void viewJetWithLongestRange() {
+
+	}
+
+	public void loadAllCargoPlanes() {
+
+	}
+
+	public void callAllFighterJetsToFight() {
+
+	}
+
+	public void addAJetToFleet() {
+
+	}
+
+	public void removeAJetFromFleet() {
+
+	} 
+
+}
